e2adce16a7e4407fb816d8c8d74d35fc6381c641
miteCrayon <dydqja0476@naver.com>
2022-12-15 18:28:36 +0900

    musinsa crawl

--- static/index.js
+++ static/index.js
@@ -1,16 +1,23 @@
-const login_btn = document.getElementById("login_btn");
-const register_btn = document.getElementById("register_btn");
+const local_id = localStorage.getItem('id');
+const local_pw = localStorage.getItem('pw');
 const id_form = document.getElementById("id_form");
 const pw_form = document.getElementById("pw_form");
+const login_btn = document.getElementById("login_btn");
 const start_text = document.getElementById("start_text");
+const register_btn = document.getElementById("register_btn");
 const default_id = ['test_id', 'admin_id', 'test1234'];
 const default_pw = ['test_pw', 'admin_pw', 'password1234'];
 
-login_btn.addEventListener("click", e => {
+if (default_id.includes(local_id) && default_pw.includes(local_pw)) {
+  console.log(local_id);
+  alert(`${local_id}로 자동 로그인됐습니다!`);
+  location.href = '../templates/mainpage.html'
+}
+
+login_btn.addEventListener("click", e => { 
   e.preventDefault();
   const user_id = id_form.value;
   const user_pw = pw_form.value;
-  const local_id = localStorage.getItem('id');
   console.log(local_id);
   if (localStorage.getItem('id') === null) {
     if (user_id !== '' && user_pw !== '') {
@@ -24,13 +31,12 @@ login_btn.addEventListener("click", e => {
         console.log(localStorage);
         alert(`${user_id}로 로그인됐습니다!`);
         location.href = '../templates/mainpage.html'
+      } else {
+        alert('아이디와 비밀번호를 정확히 입력해주세요!');
       }
     } else {
       alert('아이디와 비밀번호를 입력해주세요!');
     } 
-  } else {
-    alert(`${local_id}로 자동 로그인됐습니다!`);
-    location.href = '../templates/mainpage.html'
   }
 });
 
